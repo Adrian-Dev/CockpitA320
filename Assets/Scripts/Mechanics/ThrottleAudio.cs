d084eda880a3c7be2399d68c3344cfe6c85b0e3f
Adrian-Developer <soft.developer.adrian@gmail.com>
2021-01-05 15:20:04 +0100

    * Improves throttle sound mechanic

--- Assets/Scripts/Mechanics/ThrottleAudio.cs
+++ Assets/Scripts/Mechanics/ThrottleAudio.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class ThrottleAudio : MonoBehaviour
 {
-    [SerializeField] AudioClip _audioTurbineIdle;
-    [SerializeField] AudioClip _audioTurbineContinuous;
-    [SerializeField] AudioClip _audioTurbineAccelerate;
-    [SerializeField] AudioClip _audioTurbineDecelerate;
-    [SerializeField] AudioClip _audioTurbineContinuousReverse;
-    [SerializeField] AudioClip _audioTurbineAccelerateReverse;
-    [SerializeField] AudioClip _audioTurbineDecelerateReverse;
+    [SerializeField] AudioClip _audioAccelerationFromIdleToClimb;
+    [SerializeField] AudioClip _audioAccelerationClimbConstante;
+    [SerializeField] AudioClip _audioDecelerationFromClimbToIdle;
+    [SerializeField] AudioClip _audioAccelerationFromClimbToMaxCont;
+    [SerializeField] AudioClip _audioAccelerationMaxContConstante;
+    [SerializeField] AudioClip _audioDecelerationFromMaxContToClimb;
+    [SerializeField] AudioClip _audioAccelerationFromMaxContToMaxTO;
+    [SerializeField] AudioClip _audioAccelerationMaxTOConstante;
+    [SerializeField] AudioClip _audioDecelerationFromMaxTOToMaxCont;
+    [SerializeField] AudioClip _audioAccelerationFromIdleToMaxReverse;
+    [SerializeField] AudioClip _audioAccelerationMaxReverseConstante;
+    [SerializeField] AudioClip _audioDecelerationFromMaxReverseToIdle;
 
     AudioSource _audioSource;
-
     ThrottlePosition _throttlePosition;
 
     void Awake()
@@ -31,46 +35,93 @@ public class ThrottleAudio : MonoBehaviour
     {
         if (_throttlePosition.CurrentPosition != _throttlePosition.PreviousPosition)
         {
-            if (_throttlePosition.CurrentPosition > ThrottlePosition.Position.Idle &&
-                _throttlePosition.PreviousPosition == ThrottlePosition.Position.Idle) // Acceleration
+            if (_throttlePosition.CurrentPosition == ThrottlePosition.Position.MaxClimb &&
+                _throttlePosition.PreviousPosition == ThrottlePosition.Position.Idle) 
             {
-                _audioSource.clip = _audioTurbineAccelerate;
+                _audioSource.clip = _audioAccelerationFromIdleToClimb;
                 _audioSource.loop = false;
                 _audioSource.Play();
                 yield return new WaitUntil(() => (!_audioSource.isPlaying));
 
-                _audioSource.clip = _audioTurbineContinuous;
+                _audioSource.clip = _audioAccelerationClimbConstante;
                 _audioSource.loop = true;
                 _audioSource.Play();
             }
-            else if (_throttlePosition.CurrentPosition < ThrottlePosition.Position.Idle &&
-                _throttlePosition.PreviousPosition == ThrottlePosition.Position.Idle) // Max Reverse Acceleration
+            else if (_throttlePosition.CurrentPosition == ThrottlePosition.Position.MaxContinuous &&
+                _throttlePosition.PreviousPosition == ThrottlePosition.Position.MaxClimb)
             {
-                _audioSource.clip = _audioTurbineAccelerateReverse;
+                _audioSource.clip = _audioAccelerationFromClimbToMaxCont;
                 _audioSource.loop = false;
                 _audioSource.Play();
                 yield return new WaitUntil(() => (!_audioSource.isPlaying));
 
-                _audioSource.clip = _audioTurbineContinuousReverse;
+                _audioSource.clip = _audioAccelerationMaxContConstante;
                 _audioSource.loop = true;
                 _audioSource.Play();
             }
-            else if (_throttlePosition.PreviousPosition > ThrottlePosition.Position.Idle &&
-                _throttlePosition.CurrentPosition == ThrottlePosition.Position.Idle)
+            else if (_throttlePosition.CurrentPosition == ThrottlePosition.Position.MaxTakeOff &&
+                    _throttlePosition.PreviousPosition == ThrottlePosition.Position.MaxContinuous)
             {
-                _audioSource.clip = _audioTurbineDecelerate;
+                _audioSource.clip = _audioAccelerationFromMaxContToMaxTO;
                 _audioSource.loop = false;
                 _audioSource.Play();
-               // yield return new WaitUntil(() => (!_audioSource.isPlaying));
+                yield return new WaitUntil(() => (!_audioSource.isPlaying));
+
+                _audioSource.clip = _audioAccelerationMaxTOConstante;
+                _audioSource.loop = true;
+                _audioSource.Play();
             }
-            else if (_throttlePosition.PreviousPosition < ThrottlePosition.Position.Idle &&
-                _throttlePosition.CurrentPosition == ThrottlePosition.Position.Idle)
+            else if (_throttlePosition.CurrentPosition == ThrottlePosition.Position.MaxContinuous &&
+                _throttlePosition.PreviousPosition == ThrottlePosition.Position.MaxTakeOff)
             {
-                _audioSource.clip = _audioTurbineDecelerateReverse;
+                _audioSource.clip = _audioDecelerationFromMaxTOToMaxCont;
                 _audioSource.loop = false;
                 _audioSource.Play();
-              // yield return new WaitUntil(() => (!_audioSource.isPlaying));
+                yield return new WaitUntil(() => (!_audioSource.isPlaying));
+
+                _audioSource.clip = _audioAccelerationMaxContConstante;
+                _audioSource.loop = true;
+                _audioSource.Play();
             }
+            else if (_throttlePosition.CurrentPosition == ThrottlePosition.Position.MaxClimb &&
+                _throttlePosition.PreviousPosition == ThrottlePosition.Position.MaxContinuous)
+            {
+                _audioSource.clip = _audioDecelerationFromMaxContToClimb;
+                _audioSource.loop = false;
+                _audioSource.Play();
+                yield return new WaitUntil(() => (!_audioSource.isPlaying));
+
+                _audioSource.clip = _audioAccelerationClimbConstante;
+                _audioSource.loop = true;
+                _audioSource.Play();
+            }
+            else if (_throttlePosition.CurrentPosition == ThrottlePosition.Position.Idle &&
+                _throttlePosition.PreviousPosition == ThrottlePosition.Position.MaxClimb)
+            {
+                _audioSource.clip = _audioDecelerationFromClimbToIdle;
+                _audioSource.loop = false;
+                _audioSource.Play();
+            }
+            else if (_throttlePosition.CurrentPosition == ThrottlePosition.Position.MaxReverse &&
+                _throttlePosition.PreviousPosition == ThrottlePosition.Position.Idle)
+            {
+                _audioSource.clip = _audioAccelerationFromIdleToMaxReverse;
+                _audioSource.loop = false;
+                _audioSource.Play();
+                yield return new WaitUntil(() => (!_audioSource.isPlaying));
+
+                _audioSource.clip = _audioAccelerationMaxReverseConstante;
+                _audioSource.loop = true;
+                _audioSource.Play();
+            }
+            else if (_throttlePosition.CurrentPosition == ThrottlePosition.Position.Idle &&
+                _throttlePosition.PreviousPosition == ThrottlePosition.Position.MaxReverse)
+            {
+                _audioSource.clip = _audioDecelerationFromMaxReverseToIdle;
+                _audioSource.loop = false;
+                _audioSource.Play();
+            }
+
         }
 
         yield return null;
